20d915fbe875cd1a0c7ba1670017e8f01f103d5f
Mostak Mahmud Raihan <m69450121@gmail.com>
2026-04-21 11:03:57 +0600

    express server created

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to database
+// connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
