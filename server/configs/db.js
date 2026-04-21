8e8e9c939312ac383712afc13fdc0a62cd90f2bf
Mostak Mahmud Raihan <m69450121@gmail.com>
2026-04-21 13:52:49 +0600

    recheked

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,9 +1,12 @@
+import mongoose from "mongoose";
+
 const connectDB = async () => {
     try {
+        console.log("MONGODB_URI:", process.env.MONGODB_URI ? "is set" : "UNDEFINED!");
         mongoose.connection.on('connected', () => console.log("Database connected!"));
-        mongoose.connection.on('error', (err) => console.error("Mongoose error:", err));
         await mongoose.connect(`${process.env.MONGODB_URI}/moonlight`);
     } catch (error) {
         console.error("ConnectDB failed:", error.message);
     }
 };
+export default connectDB;
